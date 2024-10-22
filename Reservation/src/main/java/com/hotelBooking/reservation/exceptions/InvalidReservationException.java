001d81adc8da1ef212dcf053db586a3b50145a33
PUROOCAPGEMINI <puroo.kulshrestha@capgemini.com>
2024-10-22 13:59:31 +0530

    Delete Reservation directory

--- Reservation/src/main/java/com/hotelBooking/reservation/exceptions/InvalidReservationException.java
+++
@@ -1,9 +0,0 @@
-package com.hotelBooking.reservation.exceptions;
-
-@SuppressWarnings("serial")
-public class InvalidReservationException extends RuntimeException {
-    public InvalidReservationException(String message) {
-        super(message);
-    }
-}
-
--- Reservation/src/main/java/com/hotelBooking/reservation/exceptions/RoomUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.hotelBooking.reservation.exceptions;
-
-@SuppressWarnings("serial")
-public class RoomUnavailableException extends Exception {
-    public RoomUnavailableException(String message) {
-        super(message);
-    }
-}
