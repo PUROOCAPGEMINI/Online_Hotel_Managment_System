bb2d4f54ba475602ecd0edbcb47cf8c8e7a84e21
PUROOCAPGEMINI <puroo.kulshrestha@capgemini.com>
2024-10-22 13:59:18 +0530

    Delete Payment directory

--- Payment/src/main/java/com/hotelBooking/payment/repository/PaymentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hotelBooking.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotelBooking.payment.model.Payment;
-
-public interface PaymentRepo extends JpaRepository<Payment, Integer>{
-
-}
